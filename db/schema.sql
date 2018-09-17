4bbe764851f6678cfbedc77a70dc7990f901adeb
Bee <bee406@users.noreply.github.com>
2018-09-17 13:07:04 -0600

    config and schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE safeCheck_db;
 USE safeCheck_db;
 
 CREATE TABLE `checkIns` (
+    
   `id` INT(11) AUTO_INCREMENT NOT NULL,
   `first_name` VARCHAR( 255) NOT NULL,
   `last_name` VARCHAR( 255) NOT NULL,
