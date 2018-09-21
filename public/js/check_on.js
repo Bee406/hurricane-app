e605c84101d2c6ba531daa4bc6a81de95b44bf0a
Bee <bee406@users.noreply.github.com>
2018-09-20 21:59:17 -0600

    update on check on

--- public/js/check_on.js
+++ public/js/check_on.js
@@ -17,7 +17,7 @@ $("#recentMessage").on("click", function (event) {
         else {
             $("#exampleModalLongTitle").text("Current check in status for " + data.first_name + " " + data.last_name);
             for (var i = 0; i < data.Statuses.length; i++) {
-                $("ul").prepend("<li><b>" + moment(data.Statuses[i].createdAt).format("MM/DD/YY hh:mm A") + "</b><br>" + data.Statuses[i].comments + "</>li");
+                $("ul").prepend("<li><b>" + moment(data.Statuses[i].createdAt).format("MM/DD/YY hh:mm A") + "</b><br>" + data.Statuses[i].comments + "</li>");
 
             }
         }
